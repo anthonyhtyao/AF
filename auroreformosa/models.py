21b83a73dbb9fedeb860fb82bd3b4aba92efb8d5
Yu-Lin HUANG <std610706@MacBook-Air-de-Yu-Lin-HUANG.local>
2016-02-29 21:59:31 +0100

    Change Article models for multi-languages

--- auroreformosa/models.py
+++ auroreformosa/models.py
@@ -4,19 +4,25 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    name_fr = models.CharField(max_length = 128, blank = True)
-    name_tw = models.CharField(max_length = 128, blank = True)
+    name = models.CharField(max_length = 128, blank = True)
 
     def __str__(self):
         return self.user.username
 
 class Article(models.Model):
-    title_fr = models.CharField(max_length=128)
-    title_tw = models.CharField(max_length=128)
-    author = models.ForeignKey(UserProfile)
-    content_fr = models.TextField(null=True)
-    content_tw = models.TextField(null=True)
-    date = models.DateTimeField(auto_now_add=True, auto_now=False)
+    author = models.ForeignKey(UserProfile, null=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
+    
+
+class ArticleContent(models.Model):
+    LANGUAGES = (
+        ('fr', 'Français'),
+        ('tw', '繁體中文'),
+    )
+    title = models.CharField(max_length=128)
+    content = models.TextField(null=True)
+    language = models.CharField(max_length=2, choices=LANGUAGES, default='fr')
+
 
     def __str__(self):
-        return self.title_fr
+        return self.title

07d11982dd5f87702b6464b089455592a5c5d747
Snoopy <anthonyhtyao@gmail.com>
2016-03-02 16:35:00 +0100

    Add upload model

--- auroreformosa/views.py
+++ auroreformosa/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from auroreformosa.models import *
+from auroreformosa.forms import *
+from django.http import HttpResponseRedirect
 
 def index(request):
 	return render(request, 'AF/index.html')
+
+def uploadImg(request):
+    if request.method == 'POST':
+        form = ImgForm(request.POST, request.FILES)
+        if form.is_valid():
+            newImg = Img(imgfile = request.FILEs['imgfile'])
+            newImg.save()
+    return HttpResponseRedirect('/')

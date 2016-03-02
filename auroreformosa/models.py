027b00e73036df3195ecf84498e077dcc8bef850
Snoopy <anthonyhtyao@gmail.com>
2016-03-03 00:19:23 +0100

    Add category model

--- auroreformosa/models.py
+++ auroreformosa/models.py
@@ -27,3 +27,25 @@ class ArticleContent(models.Model):
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    LANGUAGES = (
+        ('fr', 'Français'),
+        ('tw', '繁體中文'),
+    )
+    TITLES = (
+        ('Politique', 'Politique'),
+        ('Recette', 'Recette'),
+        ('Societe', 'Société'),
+        ('Arts', 'Arts'),
+        ('Culture', 'Culture'),
+        ('Jeux', 'Jeux de mots'),
+        ('Dessin', 'Bande dessinée'),
+        ('Vies', 'Vies-à-vies'),
+        ('Actualite', 'Actualité')
+    )
+    title = models.CharField(max_length = 10, choices = TITLES)
+    language = models.CharField(max_length = 2, choices = LANGUAGES, default = 'fr')
+
+    def __str__(self):
+        return self.title

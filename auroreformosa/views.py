bd013df636a0fd410e0588c1a93acea691a6eda8
Snoopy <anthonyhtyao@gmail.com>
2016-03-04 12:45:33 +0100

    Put jQuery in header and add category select in article create

--- auroreformosa/views.py
+++ auroreformosa/views.py
@@ -8,4 +8,6 @@ def index(request):
 def createarticle(request):
     articleForm = ArticleForm()
     numeros = Numero.objects.all()
-    return render(request, 'AF/createArticle.html', {'form':articleForm, 'numeros':numeros})
+    categoryFR = CategoryDetail.objects.filter(language='fr')
+    categoryTW = CategoryDetail.objects.filter(language='tw')
+    return render(request, 'AF/createArticle.html', {'form':articleForm, 'numeros':numeros, 'categoryFR':categoryFR, 'categoryTW':categoryTW})

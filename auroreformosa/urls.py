12f7f29e8123f429d7fc448c5c060cae13b7d39b
Snoopy <anthonyhtyao@gmail.com>
2016-03-05 12:45:42 +0100

    Add category page's views

--- auroreformosa/urls.py
+++ auroreformosa/urls.py
@@ -3,6 +3,7 @@ from auroreformosa import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^upload', views.uploadImg, name='upload')
+    url(r'^upload', views.uploadImg, name='upload'),
     url(r'^createarticle', views.createarticle, name='createarticle'),
+    url(r'^(?P<category>[a-z]+)$', views.category, name='category')
 ]

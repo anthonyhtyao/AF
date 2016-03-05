916f58278b4464b01cb620cc749323cbc17115ee
Snoopy <anthonyhtyao@gmail.com>
2016-03-05 14:36:38 +0100

    Add slug in Article model

--- auroreformosa/urls.py
+++ auroreformosa/urls.py
@@ -3,6 +3,6 @@ from auroreformosa import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^upload', views.uploadImg, name='upload')
+    url(r'^upload', views.uploadImg, name='upload'),
     url(r'^createarticle', views.createarticle, name='createarticle'),
 ]

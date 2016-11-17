bd69064620c217ba056f25b370dd058fc50f4d5c
htyao <anthonyhtyao@gmail.com>
2016-11-17 01:28:31 +0100

    Add abo class

--- abo/models.py
+++ abo/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Subscriber(models.Model):
+    CIVILITE = {('Ms','M.'),('Ml','Mlle'),('Mm','Mme')}
+    civilite = models.CharField(max_length=2, choices=CIVILITE, default='Ms')
+    family_name = models.CharField(max_length=63)
+    name = models.CharField(max_length=63)
+    email = models.EmailField(max_length=127, null=True)
+    adress = models.TextField()
+    country = models.CharField(max_length=63)
+    info = models.BooleanField(default=False)
+    remark = models.CharField(max_length=128)
+
+class Donation(models.Model):
+    donor = models.ForeignKey(Subscriber, related_name='donations')
+    amount = models.IntegerField(default=0)
+    received = models.BooleanField(default=False)
+    receipt = models.BooleanField(default=False)
+    status = models.SmallIntegerField(default=1)
+    auditor = models.ForeignKey(User)
+
+class Subscription(models.Model):
+    PAYMENT = {('Y','Yes'),('N','No'),('P','Point'),('E','Exempte')}
+    subscriber = models.ForeignKey(Subscriber, related_name='subscriptions')
+    start = models.IntegerField()
+    end = models.IntegerField()
+    quantity = models.IntegerField(default=1)
+    payment = models.CharField(max_length=1, choices=PAYMENT, default='N')
+    status = models.SmallIntegerField(default=1)
+    auditor = models.ForeignKey(User)

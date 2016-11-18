27588f3525760a6af5d9c67e80023263e9dd21d3
htyao <anthonyhtyao@gmail.com>
2016-11-18 21:36:14 +0100

    Postgres

--- AF/settings.py
+++ AF/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auroreformosa'
+    'auroreformosa',
+    'abo'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -79,8 +80,11 @@ WSGI_APPLICATION = 'AF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'af',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

0484fa368d9374c697d485c58a935d80d9b67260
htyao <anthonyhtyao@gmail.com>
2017-01-11 22:58:49 +0100

    Abo admin

--- abo/admin.py
+++ abo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from abo.models import *
 
 # Register your models here.
+
+admin.site.register(Subscriber)
+admin.site.register(Donation)
+admin.site.register(Subscription)

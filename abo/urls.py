7c2914051de928d5718b22fa37536f34eb4be1ac
htyao <anthonyhtyao@gmail.com>
2017-01-12 00:49:56 +0100

    Simple Client info

--- abo/urls.py
+++ abo/urls.py
@@ -3,4 +3,5 @@ from abo import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^client/(?P<client>[0-9]+)$', views.clientDetail, name='clientDetail'),
 ]
--- abo/views.py
+++ abo/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from django.core.mail import send_mail
-import urllib
-from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
-from django.template import Context
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-import random
-from django.core import serializers
-import json
 from django.conf import settings
+from abo.models import *
 
 @login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    returnForm = {}
+    
+    return render(request, 'abo/index.html',{})
+
+@login_required
+def clientDetail(request,client):
+    returnForm = {}
+    client = Subscriber.objects.get(id=int(client))
+    returnForm['client'] = client
+    return render(request, 'abo/clientDetail.html',returnForm)

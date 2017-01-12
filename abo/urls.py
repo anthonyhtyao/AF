e0bc79d4a7aa6611dbed396d81e3f50314dba59e
htyao <anthonyhtyao@gmail.com>
2017-01-12 21:29:08 +0100

    Add edit client info function

--- abo/urls.py
+++ abo/urls.py
@@ -4,4 +4,5 @@ from abo import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^client/(?P<client>[0-9]+)$', views.clientDetail, name='clientDetail'),
+    url(r'^client/(?P<client>[0-9]+)/edit$', views.clientEdit, name='clientEdit'),
 ]
--- abo/views.py
+++ abo/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.template.loader import get_template
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from abo.models import *
+import json
+from django.forms.models import model_to_dict
 
 @login_required
 def index(request):
@@ -22,3 +24,15 @@ def clientDetail(request,client):
     returnForm['dons'] = dons
     returnForm['subs'] = subs
     return render(request, 'abo/clientDetail.html',returnForm)
+
+@login_required
+def clientEdit(request,client):
+    client = Subscriber.objects.get(id=int(client))
+    if request.method=="POST":
+        data = json.loads(request.body.decode('utf-8'))
+        client.email = data['clientEmail']
+        client.adress = data['clientAdress']
+        client.info = 1 == data['clientInfo']
+        client.remark = data['clientRemark']
+        client.save()
+        return HttpResponse(1)
